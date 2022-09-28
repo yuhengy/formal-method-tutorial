69555c1346c3228c57782b07f75f16a27bf55ad5
Yuheng Yang <yuhengy@mit.edu>
2022-09-28 16:33:23 -0400

    add some doc link

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,8 @@
 
-## pwd is always in formal-method-intro folder
+## pwd is always in formal-method-tutorial folder
+
+
+
 
 ## docker
 # docker compose up -d
@@ -9,10 +12,16 @@
 # cd workspace
 
 
+
+
 ## Racket
 # raco test src/traffic-light-bounded.rkt
 # raco test src/traffic-light-unbounded1.rkt
 # raco test src/traffic-light-unbounded2.rkt
+
+## Documents for Racket and Rosette
+# https://docs.racket-lang.org
+
 
 
 
@@ -21,5 +30,8 @@
 # Ctrl+j : evaluate for next line
 # Ctil+k : un-evaluate for current line
 # \ c l (type them one-by-one) : evalutate/un-evaluate to current line of cursor
+
+## A cheat sheet for coq command I use
+# https://www.cs.cornell.edu/courses/cs3110/2018sp/a5/coq-tactics-cheatsheet.html
 
 
